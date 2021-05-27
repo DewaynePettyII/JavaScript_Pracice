6a494b26da414c4b545f348aa0cc11cbad96c63d
Dewayne Petty II <christsiblings@gmail.com>
2021-05-27 08:32:12 -0500

    first commit

---
+++ reduce.js
@@ -0,0 +1,7 @@
+//9. Turn an array of numbers into a long string of all those numbers. (use reduce)
+let stringConcat = [1, 2, 3].reduce((accumulator, currentValue, currentIndex, array) => {
+    return currentValue + currentIndex
+});
+   console.log(stringConcat); // "123"
+    
+    
